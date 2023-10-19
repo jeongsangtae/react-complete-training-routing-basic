e3e6c60549e6cdfe2f62e50185806cc58146a1cb
jeongsangtae <wjdtkdxo55@naver.com>
2023-10-20 05:18:16 +0900

    React Complete Training Routing Basic - Move between pages with Link (Home component edit)

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import {
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/products", element: <Products /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go <Link to="/products">products</Link>.
+      </p>
+    </>
+  );
 };
 
 export default Home;
